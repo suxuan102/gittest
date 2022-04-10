768f0d8c16325d0780127432e258ced02311a8f5
sjw <2226813231@qq.com>
2022-04-10 16:15:55 +0800

    master v3

--- src/main/java/com/sjw/GitTest2.java
+++ src/main/java/com/sjw/GitTest2.java
@@ -2,7 +2,7 @@ package com.sjw;
 
 public class GitTest2 {
     public static void main(String[] args) {
-        System.out.println("hello git！");
+        System.out.println("hello git！001");
         System.out.println("hello git！");
         System.out.println("hello git！3333333");
     }
