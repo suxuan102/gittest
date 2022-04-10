2b7928ec205d0bb61f0dffe5469ec12bbb4b27c7
suxuan102 <86296360+suxuan102@users.noreply.github.com>
2022-04-10 17:02:56 +0800

    Update GitTest2.java
    pull test

--- src/main/java/com/sjw/GitTest2.java
+++ src/main/java/com/sjw/GitTest2.java
@@ -4,7 +4,7 @@ public class GitTest2 {
     public static void main(String[] args) {
         System.out.println("hello git！001");
         System.out.println("hello git！002");
-        System.out.println("hello git！3333333");
+        System.out.println("hello git！pull");
         System.out.println("hello git！push");
     }
 }
