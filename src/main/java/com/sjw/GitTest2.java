17def972ec79389154f6f0b8d73fab3987dcab5c
sjw <2226813231@qq.com>
2022-04-10 15:42:36 +0800

    v2

--- src/main/java/com/sjw/GitTest2.java
+++ src/main/java/com/sjw/GitTest2.java
@@ -4,5 +4,6 @@ public class GitTest2 {
     public static void main(String[] args) {
         System.out.println("hello git！");
         System.out.println("hello git！");
+        System.out.println("hello git！");
     }
 }
